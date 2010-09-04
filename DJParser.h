d6be9da6d2acb50631a35c5ef12a4b88fdbfcb48
Jerome Poichet <poitch@gmail.com>
2010-09-03 21:04:46 -0700

    latest version

--- DJParser.h
+++ DJParser.h
@@ -5,10 +5,23 @@
 //  Created by Jerome Poichet on 1/6/10.
 //  Copyright 2010 Jerome Poichet. All rights reserved.
 //
+//  License:
+//      You can use the code in your own products
+//      You can modify the code as you wish
+//      You can use modified code in your products
+//      You can redistribute the original code but retain that copyright notice
+//      I’m not liable for anything you do with the code, no matter what.
+//      You can’t use my name or other marks to promote your products based on the code.
+//      Attribution in your product is welcome but not required
+//
+//
 
-//#import <Cocoa/Cocoa.h>
-#import <UIKit/UIKit.h>
-
+#if TARGET_OS_IPHONE
+    #import <Foundation/Foundation.h>
+    #import <UIKit/UIKit.h>
+#else
+    #import <Cocoa/Cocoa.h>
+#endif
 
 @interface DJParser : NSObject {
     NSString *json;
--- DJParser.m
+++ DJParser.m
@@ -5,6 +5,15 @@
 //  Created by Jerome Poichet on 1/6/10.
 //  Copyright 2010 Jerome Poichet. All rights reserved.
 //
+//  License:
+//      You can use the code in your own products
+//      You can modify the code as you wish
+//      You can use modified code in your products
+//      You can redistribute the original code but retain that copyright notice
+//      I’m not liable for anything you do with the code, no matter what.
+//      You can’t use my name or other marks to promote your products based on the code.
+//      Attribution in your product is welcome but not required
+//
 
 #import "DJParser.h"
 
